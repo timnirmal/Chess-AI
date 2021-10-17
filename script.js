22e7e81deee304b1422844f097dd47dc160a645b
thimira nirmal <timnirmal@gmail.com>
2021-10-17 12:46:00 +0530

    Highlight Legal Moves

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 var board = null
 var game = new Chess()
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
 
 var $status = $('#status')
 var $fen = $('#fen')
 var $pgn = $('#pgn')
 
 
-var onMouseoverSquare = function(square, piece) {
-
-};
-
-var onMouseoutSquare = function(square, piece) {
-
-};
-
-function onDragStart (source, piece, position, orientation) {
-    // do not pick up pieces if the game is over
-    if (game.game_over()) return false
-
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-        return false
-    }
+// Highlight Legal Moves
+function removeGreySquares () {
+    $('#board .square-55d63').css('background', '');
 }
 
+function greySquare (square) {
+    var squareHi = $('#board .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareHi.hasClass('black-3c85d') === true) {
+        background = '#696969';
+    }
+    squareHi.css('background', background);
+}
+
+// Random Moves
 function makeRandomMove () {
     var possibleMoves = game.moves()
 
@@ -34,35 +33,9 @@ function makeRandomMove () {
     var randomIdx = Math.floor(Math.random() * possibleMoves.length)
     game.move(possibleMoves[randomIdx])
     board.position(game.fen())
-
-    // This will make a loop
-    // 250 (timeout) can be changed for faster game
-    window.setTimeout(makeRandomMove, 250)
 }
 
-function onDrop (source, target) {
-    // see if the move is legal
-    var move = game.move({
-        from: source,
-        to: target,
-        promotion: 'q' // Promote porn to Queen
-    })
-
-    // illegal move
-    if (move === null) return 'snapback'
-
-    // make random legal move for black
-    // Keeping this will let you pick the fist move if you need all random remove below line
-    window.setTimeout(makeRandomMove, 250)
-
-    updateStatus()
-}
-
-// update the board position after the piece snap for castling, en passant, pawn promotion
-function onSnapEnd () {
-    board.position(game.fen())
-}
-
+// Game State
 function updateStatus () {
     var status = ''
 
@@ -96,12 +69,74 @@ function updateStatus () {
     $pgn.html(game.pgn())
 }
 
+///// Chessboardjs Functions
+
+function onMouseoverSquare (square, piece) {
+    // get list of possible moves for this square
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    })
+
+    // exit if there are no moves available for this square
+    if (moves.length === 0) return
+
+    // highlight the square they moused over
+    greySquare(square)
+
+    // highlight the possible squares for this piece
+    for (var i = 0; i < moves.length; i++) {
+        console.log(moves[i].to);
+        greySquare(moves[i].to)
+    }
+}
+
+function onMouseoutSquare (square, piece) {
+    removeGreySquares()
+}
+
+function onDragStart (source, piece, position, orientation) {
+    // do not pick up pieces if the game is over
+    if (game.game_over()) return false
+
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        return false
+    }
+}
+
+function onDrop (source, target) {
+    removeGreySquares()
+
+    // see if the move is legal
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q' // Promote porn to Queen
+    })
+
+    // illegal move
+    if (move === null) return 'snapback'
+
+    // make random legal move for black
+    window.setTimeout(makeRandomMove, 250)
+
+    updateStatus()
+}
+
+// update the board position after the piece snap for castling, en passant, pawn promotion
+function onSnapEnd () {
+    board.position(game.fen())
+}
 
 var config = {
     draggable: true,
     position: 'start',
     onDragStart: onDragStart,
     onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
     onSnapEnd: onSnapEnd
 };
 
